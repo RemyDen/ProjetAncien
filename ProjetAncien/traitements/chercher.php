c9aa9e904a5c63f65281316b21595825fbe66717
PoloRelou <paulnaszalyi@yahoo.fr>
2018-05-17 17:36:34 +0200

    Add files via upload

--- ProjetAncien/traitements/chercher.php
+++ ProjetAncien/traitements/chercher.php
@@ -7,11 +7,15 @@
  */
 
 if(isset($_GET['recherche'])) {
-    $recherche = $_GET['recherche'];
+    if(strlen($_GET['recherche']) <= 2) {
+?>
+        <script>alert("Votre recherche doit comporter au moins 3 caractères")</script>
+<?php
+    } else {
+        $recherche = $_GET['recherche'];
 
-    $req = "SELECT * FROM utilisateur WHERE nom LIKE '%".$recherche."%' OR prenom LIKE '%".$recherche."%' OR ville LIKE '%".$recherche."%' OR entreprise LIKE '%".$recherche."%' OR poste LIKE '%".$recherche."%'";
-    $exe = $bdd->query($req);
-    $res = $exe->fetchAll(PDO::FETCH_ASSOC);
-
-    var_dump($res);
+        $req = "SELECT * FROM utilisateur WHERE typeUtilisateur = 2 AND (nom LIKE '%" . $recherche . "%' OR prenom LIKE '%" . $recherche . "%' OR ville LIKE '%" . $recherche . "%' OR entreprise LIKE '%" . $recherche . "%' OR poste LIKE '%" . $recherche . "%')";
+        $exe = $bdd->query($req);
+        $res = $exe->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
